f2de69cad4b41ac69dc859b3718f3aa2f6d11226
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-10-07 11:29:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-project
+ Este projeto consiste em uma interface de navegação responsiva desenvolvida com Next.js, React e Tailwind CSS.  A barra de navegação é totalmente responsiva.
---
+++ src/app/constants/icons.ts
@@ -0,0 +1,9 @@
+export const ICONS = {   
+        bell: 'icons/sino.svg',
+        close: 'icons/close.svg',
+        flag: 'icons/bandeira.svg',
+        home: 'icons/home.svg',
+        menu: 'icons/menu.svg',
+        person: 'icons/person.svg',
+        logo: '/icons/logo.png',
+};
